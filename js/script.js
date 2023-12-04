6381f1d49b70ce491829f54cb395708a5bae5678
Gerard Press <killerabs@gmail.com>
2023-12-04 06:34:48 +0000

    current time

--- js/script.js
+++ js/script.js
@@ -19,3 +19,23 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+//CLOCK
+const timeElement = document.getElementById("clock");
+
+function updateTime() {
+  const now = new Date();
+  const hours = now.getHours();
+  const minutes = now.getMinutes();
+  const seconds = now.getSeconds();
+
+  // Format the string with leading zeroes
+  const clockStr = `${hours.toString().padStart(2, "0")}:${minutes
+    .toString()
+    .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+
+  timeElement.innerText = clockStr;
+}
+
+updateTime();
+setInterval(updateTime, 1000);
